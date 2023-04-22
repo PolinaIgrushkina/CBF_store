7f40719848e319ae3ae3901dc25de7b00719dbd9
Polina Igrushkina <polina.igrushkina@gmail.com>
2023-04-22 11:58:12 +0300

    Changed header's styles, made hero + slider

---
+++ js/hero-slider.js
@@ -0,0 +1,63 @@
+const sliderImages = [
+  "images/hero-1x.jpg",
+  "images/hero2-1x.jpg",
+  "images/hero-1x.jpg",
+];
+
+const slider = document.querySelector(".slider");
+const sliderImage = slider.querySelector(".slider-image");
+const prevBtn = slider.querySelector(".slider-btn-prev");
+const nextBtn = slider.querySelector(".slider-btn-next");
+const dotsContainer = slider.querySelector(".slider-dots");
+
+let currentImageIndex = 0;
+
+function showCurrentImage() {
+  sliderImage.style.backgroundImage = `url(${sliderImages[currentImageIndex]})`;
+  updateDots();
+}
+
+function updateDots() {
+  const dots = dotsContainer.querySelectorAll(".slider-dot");
+  dots.forEach((dot, index) => {
+    if (index === currentImageIndex) {
+      dot.classList.add("active");
+    } else {
+      dot.classList.remove("active");
+    }
+  });
+}
+
+function nextImage() {
+  currentImageIndex++;
+  if (currentImageIndex >= sliderImages.length) {
+    currentImageIndex = 0;
+  }
+  showCurrentImage();
+}
+
+function prevImage() {
+  currentImageIndex--;
+  if (currentImageIndex < 0) {
+    currentImageIndex = sliderImages.length - 1;
+  }
+  showCurrentImage();
+}
+
+prevBtn.addEventListener("click", prevImage);
+nextBtn.addEventListener("click", nextImage);
+
+sliderImages.forEach((_, index) => {
+  const dot = document.createElement("span");
+  dot.classList.add("slider-dot");
+  if (index === 0) {
+    dot.classList.add("active");
+  }
+  dot.addEventListener("click", () => {
+    currentImageIndex = index;
+    showCurrentImage();
+  });
+  dotsContainer.appendChild(dot);
+});
+
+showCurrentImage();
