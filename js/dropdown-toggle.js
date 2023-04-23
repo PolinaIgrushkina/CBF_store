905a33d066fec689fe3d98b40d8aedf0bd386c71
Polina Igrushkina <polina.igrushkina@gmail.com>
2023-04-24 01:18:35 +0300

    Fixes script, styles

--- js/dropdown-toggle.js
+++ js/dropdown-toggle.js
@@ -1,10 +1,28 @@
 const categoriesList = document.querySelector(".categories-list");
 const dropdownMenu = document.querySelector(".dropdown-menu");
+let selectedLi = null;
 
-categoriesList.addEventListener("click", function () {
-  if (dropdownMenu.style.display === "flex") {
-    dropdownMenu.style.display = "none";
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("categories-item")) {
+    if (event.target === selectedLi) {
+      dropdownMenu.style.display = "none";
+      selectedLi = null;
+    } else {
+      dropdownMenu.style.display = "flex";
+      selectedLi = event.target;
+    }
   } else {
-    dropdownMenu.style.display = "flex";
+    dropdownMenu.style.display = "none";
+    selectedLi = null;
+  }
+
+  if (
+    dropdownMenu.style.display === "flex" &&
+    !event.target.classList.contains("categories-item")
+  ) {
+    dropdownMenu.style.display = "none";
+    setTimeout(function () {
+      dropdownMenu.style.display = "flex";
+    }, 0);
   }
 });
